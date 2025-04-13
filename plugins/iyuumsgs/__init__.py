8c0c21c8f483d099bc61d846edbebb52cc450e5d
BlueSky <53138707+TouchBlueSky@users.noreply.github.com>
2025-04-13 17:19:42 +0800

    Update __init__.py

--- plugins/iyuumsgs/__init__.py
+++ plugins/iyuumsgs/__init__.py
@@ -19,7 +19,7 @@ class IyuuMsgs(_PluginBase):
     # 插件图标
     plugin_icon = "Iyuu_A.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "waterz"
     # 作者主页
