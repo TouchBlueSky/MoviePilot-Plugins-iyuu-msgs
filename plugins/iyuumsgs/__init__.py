0eb898d95eb324b330f6cda6f0b8650811e08a49
BlueSky <53138707+TouchBlueSky@users.noreply.github.com>
2025-04-13 16:38:45 +0800

    Update __init__.py

--- plugins/iyuumsgs/__init__.py
+++ plugins/iyuumsgs/__init__.py
@@ -27,7 +27,7 @@ class IyuuMsgs(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "iyuumsgs_"
     # 加载顺序
-    plugin_order = 96
+    plugin_order = 66
     # 可使用的用户级别
     auth_level = 1
 
